575284d039d02539437d671a9a00536f3d57a294
김정혁 <jack1150@naver.com>
2023-04-03 13:13:33 +0900

    Browser router 실습
    Browser router 실습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Board from './components/Board';
+import Profile from './components/Profile';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">홈 페이지 이동</Link>
+          </li>
+          <li>
+            <Link to="/profile">프로필 페이지 이동</Link>
+          </li>
+          <li>
+            <Link to="/board">게시판 페이지 이동</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
     </div>
   );
 }
