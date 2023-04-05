419658e44bd9a6b1a2c0120e6c73b670a6b4ee8b
김정혁 <jack1150@naver.com>
2023-04-05 11:06:43 +0900

    redux 마무리
    redux 마무리

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -12,7 +12,13 @@ const initState = {
     },
     { id: 2, text: '프로젝트 잘 마무리', done: false },
   ],
+  buyList: ['닌텐도', '자동자'],
+  todoListCount: 3,
 };
+
+let counts = initState.todoList.length;
+initState['nextID'] = counts;
+
 // 액션 타입 정의하기
 const CREATE = 'todo/CREATE';
 const DONE = 'todo/DONE';
@@ -36,14 +42,36 @@ export default function todo(state = initState, action) {
     case CREATE:
       return {
         ...state,
-        todoList: state.todoList.concat({
-          id: action.payload.id,
-          text: action.payload.text,
-          done: false,
-        }),
+        // todoList: state.todoList.concat({
+        //   id: action.payload.id,
+        //   text: action.payload.text,
+        //   done: false,
+        // }),
+        todoList: [
+          ...state.todoList,
+          {
+            id: action.payload.id,
+            text: action.payload.text,
+            done: false,
+          },
+        ],
+        nextID: action.payload.id + 1,
       };
     case DONE:
-      return console.log('DONE 호출');
+      return {
+        ...state,
+        todoList: state.todoList.map((el) => {
+          // map 함수는 반드시 return을 해줘야 배열로 반환시켜준다
+          if (el.id === action.id) {
+            return {
+              ...el, //el에 있던 나머지 구문은 그대로 return시켜 주세요
+              done: true,
+            };
+          } else {
+            return el;
+          }
+        }),
+      };
     default:
       return state;
   }
